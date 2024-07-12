20a89127ee54da459d07784a0047ffe415077902
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-07-12 15:49:26 +0100

    Update setup_logging.sh
    Removed FILES_DIR, used only SCRIPT_DIR

--- setup_logging.sh
+++ setup_logging.sh
@@ -3,18 +3,18 @@ echo "---------------------"
 echo "Setting Up Logging"
 echo "---------------------"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-FILES_DIR=${SCRIPT_DIR}/setup_files
+
 echo "Installing files from:"
-echo $FILES_DIR
+echo $SCRIPT_DIR
 echo "---------------------"
 sudo apt-get install rsyslog logrotate -y
 
 sudo install -d -m 755 -o root -g adm /var/log/containers
 
-sudo cp $FILES_DIR/syslog.conf /etc/rsyslog.d/40-docker.conf
+sudo cp $SCRIPT_DIR/syslog.conf /etc/rsyslog.d/40-docker.conf
 sudo systemctl restart rsyslog
 
-sudo cp $FILES_DIR/logrotate /etc/logrotate.d/docker
+sudo cp $SCRIPT_DIR/logrotate /etc/logrotate.d/docker
 sudo mkdir -p /etc/cron.hourly
 sudo mv /etc/cron.daily/logrotate /etc/cron.hourly/logrotate
 
