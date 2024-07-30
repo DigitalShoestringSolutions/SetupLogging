9c5a1557fdc2920b0b4aea1fe115a1ec7db21b3a
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-07-30 15:30:08 +0100

    Feature/recipe (#6)
    * Create init_SM.sh, which calls setup_logging.sh
    
    * Made init_SM executable
    
    * Added hashbang to init_SM
    
    * Removed unnecessary sleep from setup_logging.sh

---
+++ init_SM.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# init_SM.sh
+# initialisation scipt run on download of this Service Module
+
+# Get location of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Take init action:
+$SCRIPT_DIR/setup_logging.sh
